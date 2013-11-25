4643d677da0cf7a59da081d5a2b1dc683a906191
wang zhenchuan <wangzhenchuan@gmail.com>
2013-11-25 18:39:59 +0800

    avril commit

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ avril.cache = require('./lib/avril.cache');
 
 avril.mvc = require('./lib/avril.mvc');
 
-avril.localize = require('./lib/avril.localize');
-
 avril.mvc.minify = require('./lib/avril.mvc.minify');
+
+avril.localize = require('./lib/avril.localize');
 
 avril.auth = require('./lib/avril.auth');
 
 avril.mongoose = require('./lib/avril.mongoose');
-
-avril.lessdev = require('./lib/avril.lessdev');
 
 module.exports = avril;

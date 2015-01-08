0aafe306d3006bec33d7223d1d869031505c6515
Judd Trump <wangzhenchuan@gmail.com>
2015-01-08 20:32:30 +0800

    save

--- test/avril.mvc.test.js
+++ test/avril.mvc.test.js
@@ -1,3 +1,15 @@
 /**
  * Created by trump on 15-1-7.
  */
+var avril = require('../index');
+
+describe('avril.mvc', function(){
+    var queue = avril.simpleQueue();
+
+    queue.func(function(next){
+        next();
+    });
+
+    it('')
+});
+
--- test/testSite/controllers/home.js
+++ test/testSite/controllers/home.js
@@ -1,3 +1,11 @@
 /**
  * Created by trump on 15-1-7.
  */
+module.exports = {
+   index: function(req, res, next, helper) {
+       res.end('hello avril');
+   }
+    , main: function(req, res, next, helper) {
+        res.view();
+    }
+};

30baa471be1a23912da5466ecdc0f99df90a90f4
trump.wang <trump.wang@ringcentral.com>
2015-03-18 14:28:23 +0800

    add apiBaseMethod

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 avril.config = require('./package.json');
 
+avril.models = require('./lib/avril.models')
+
 avril.cache = require('./lib/avril.cache');
 
 avril.mvc = require('./lib/avril.mvc');
+
+avril.mvc.simpleApiBase = require('./lib/avril.mvc.smipleApiBase');
 
 avril.mvc.minify = require('./lib/avril.mvc.minify');
 
